27860dde1a90514de16a75f667ee6d7a6ae53a4c
Filipe Vidal <vidaltecst@gmail.com>
2022-07-01 23:26:27 -0300

    Foi feito a requisição para listar os pokemons

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,23 +1,32 @@
 import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import {PokemonsContext} from "./context/PokemonsContext";
+import { PokemonsContext } from "./context/PokemonsContext";
 import Home from './pages/Home';
 import Details from './pages/Details';
 import Pokedex from './pages/Pokedex';
+import axios from 'axios';
 
 function App() {
-  const [pokemons, setPokemons] = useState([{name: "buba"}])
+  const [pokemons, setPokemons] = useState([])
+  useState(() => {
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=20')
+    .then((res) => {
+      setPokemons(res.data.results)
+    }).catch(() => {
+
+    })
+  }, [])
   return (
     <div className="App">
-      <PokemonsContext.Provider value={pokemons}>
+      {pokemons.length&&<PokemonsContext.Provider value={pokemons}>
         <BrowserRouter>
           <Routes>
             <Route path={'/'} element={<Home/>}/>
             <Route path={'/pokedex'} element={<Pokedex/>}/>
-            <Route path={'/details'} element={<Details/>}/>
+            <Route path={'/:name'} element={<Details/>}/>
           </Routes>
         </BrowserRouter>
-      </PokemonsContext.Provider>
+      </PokemonsContext.Provider>}
     </div>
   );
 }
