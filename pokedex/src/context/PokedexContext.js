d48f71b77e45e52b9efaa4fb21b17505fc214761
Daniel Matos <danielsegatto5@gmail.com>
2022-07-07 22:45:46 -0300

    correção de erro ao adicionar pokemon

--- pokedex/src/context/PokedexContext.js
+++ pokedex/src/context/PokedexContext.js
@@ -2,13 +2,12 @@ import { createContext, useState } from "react";
 
 export const PokedexContext = createContext();
 
-export const PokedexProvider = ({children}) => {
-  const [pokedex, setPokedex] = useState([])
-  
+export const PokedexProvider = ({ children }) => {
+  const [pokedex, setPokedex] = useState([]);
+
   return (
     <PokedexContext.Provider value={[pokedex, setPokedex]}>
       {children}
     </PokedexContext.Provider>
-  )
-}
-
+  );
+};
